5b1edbbd65dd4f7fe502c2764a4f1744a5ef8f5d
Vid Rajtmajer <vid@irnas.eu>
2020-10-01 14:53:30 +0200

    manufacturer data display rework

--- components/ListDeviceItem.js
+++ components/ListDeviceItem.js
@@ -3,63 +3,53 @@ import { DecodeBase64 } from '../Helpers';
 import { View, Text, TouchableHighlight, StyleSheet } from 'react-native';
 
 const ListDeviceItem = (props) => {
-    let item_view = (
-        <View>
-            <Text style={styles.title}>
-                {props.item_in.name}  {props.item_in.id}   rssi: {props.item_in.rssi} dBm
-            </Text>
-        </View>
-        );
-    
+    let text_default, text_line1, text_line2, text_raw;
+    text_default = (    // print default info (name, mac, rssi)
+        <Text style={styles.title}>
+            {props.item_in.name}  {props.item_in.id}   rssi: {props.item_in.rssi} dBm
+        </Text>
+    );
+
     if (props.item_in.manufacturerData) {
         let decoded_raw_data = DecodeBase64(props.item_in.manufacturerData);
         //console.log(decoded_raw_data);
         let raw_data = decoded_raw_data.toJSON();
         if (props.item_in.name !== null && props.item_in.name.includes(props.filter_name)) {
+            
+            text_raw = (    // print raw manufacturer data in any case
+                <Text key="text_raw" style={styles.subtitle}>
+                    Raw data: {raw_data.data.toString()}
+                </Text>
+            );
+            
             if (props.item_in.name.includes("iz")) {    // TODO parse leakage, op mode and connection
-                item_view = 
-                <View>
-                    <Text style={styles.title}>
-                    {props.item_in.name}  {props.item_in.id}   rssi: {props.item_in.rssi} dBm
-                    </Text>
+                text_line1 = (
                     <Text style={styles.subtitle}>
                         Leakage: {raw_data.data[2]} {raw_data.data[3]}    Surge: {raw_data.data[4]}    Battery: {raw_data.data[5]} mV
                     </Text>
+                );
+                text_line2 = (
                     <Text style={styles.subtitle}>
                         Op mode: {raw_data.data[6]}   Connection: {raw_data.data[7]}
                     </Text>
-                    <Text style={styles.subtitle}>
-                        Raw data: {raw_data.data.toString()}
-                    </Text>
-                </View>;
+                );
             }
             else if (props.item_in.name === "TestDomey") {  // TODO print raw data as hex
-                item_view = 
-                <View>
-                    <Text style={styles.title}>
-                        {props.item_in.name}  {props.item_in.id}   rssi: {props.item_in.rssi} dBm
-                    </Text>
+                text_line1 = (
                     <Text style={styles.subtitle}>
                         Alarm state: {raw_data.data[2]}
                     </Text>
-                    <Text style={styles.subtitle}>
-                        Raw data: {raw_data.data.toString()}
-                    </Text>
-                </View>;
-            }
-            else {
-                item_view = 
-                <View>
-                    <Text style={styles.title}>
-                        {props.item_in.name}   {props.item_in.id}  RSSI: {props.item_in.rssi}
-                    </Text>
-                    <Text style={styles.subtitle}>
-                        Raw data: {raw_data.data.toString()}
-                    </Text>
-                </View>;
+                );
             }
         }
     }
+
+    let texts = [text_default, text_line1, text_line2, text_raw];
+    let item_view = (
+        <View>
+            { texts }
+        </View>
+    );
         
     return (
         <View style={styles.item}>
@@ -71,10 +61,6 @@ const ListDeviceItem = (props) => {
             </TouchableHighlight>
         </View>
     );
-}
-
-function parseManufacturerData() {  // TODO
-    //if ()
 }
 
 const styles = StyleSheet.create({
