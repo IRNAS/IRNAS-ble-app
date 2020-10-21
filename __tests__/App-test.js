cb99aa8b9dc574b4ad500b5278786ea6cfc9a82a
Vid Rajtmajer <vid@irnas.eu>
2020-10-21 12:24:18 +0200

    fix unit test run bug

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -14,13 +14,28 @@ it('renders correctly', () => {
 });
 */
 
+jest.mock("react-native-reanimated", () => {
+  const View = require('react-native').View;
+  return {
+    Value: jest.fn(),
+    event: jest.fn(),
+    add: jest.fn(),
+    eq: jest.fn(),
+    set: jest.fn(),
+    cond: jest.fn(),
+    interpolate: jest.fn(),
+    View: View,
+    Extrapolate: { CLAMP: jest.fn() }
+  };
+});
+
 // testing framework test
 test('Sum equals 2', () => {
       expect(1+1).toBe(2);
 });
 
 // test EncodeTrackerSettings function
-import { EncodeTrackerSettings, DecodeTrackerSettings, convertStringToChars } from '../Helpers';
+import { EncodeTrackerSettings, DecodeTrackerSettings, convertStringToChars, unpackSetting } from '../Helpers';
 
 test('encode no data ', () => {
   var input = "lr_send_interval:";   // device_command
@@ -39,7 +54,7 @@ test('encode no command ', () => {
 test('encode uint32', () => {
   var input = "lr_send_interval: 10";   // device_command
   var encoded = EncodeTrackerSettings(input);
-  var output = DecodeTrackerSettings(encoded).join(' '); // convert result array to string separated with spaces
+  var output = unpackSetting(encoded).join(' '); // convert result array to string separated with spaces
   var expectedOutput = "3 1 4 10 0 0 0";
   expect(output).toBe(expectedOutput);
 });
@@ -47,7 +62,7 @@ test('encode uint32', () => {
 test('encode uint16', () => {
   var input = "setting_name_2: 300";   // device_command
   var encoded = EncodeTrackerSettings(input);
-  var output = DecodeTrackerSettings(encoded).join(' ');
+  var output = unpackSetting(encoded).join(' ');
   var expectedOutput = "3 18 2 44 1";
   expect(output).toBe(expectedOutput);
 });
@@ -55,7 +70,7 @@ test('encode uint16', () => {
 test('encode uint8', () => {
   var input = "setting_name_1: 255";   // device_command
   var encoded = EncodeTrackerSettings(input);
-  var output = DecodeTrackerSettings(encoded).join(' ');
+  var output = unpackSetting(encoded).join(' ');
   var expectedOutput = "3 17 1 255";
   expect(output).toBe(expectedOutput);
 });
@@ -63,7 +78,7 @@ test('encode uint8', () => {
 test('encode negative int8', () => {
   var input = "setting_name_4: -4";
   var encoded = EncodeTrackerSettings(input);
-  var output = DecodeTrackerSettings(encoded).join(' ');
+  var output = unpackSetting(encoded).join(' ');
   var expectedOutput = "3 20 1 252"; // id 0x14 is 20
   expect(output).toBe(expectedOutput);
 });
@@ -71,7 +86,7 @@ test('encode negative int8', () => {
 test('encode negative int16', () => {
   var input = "setting_name_5: -300";
   var encoded = EncodeTrackerSettings(input);
-  var output = DecodeTrackerSettings(encoded).join(' ');
+  var output = unpackSetting(encoded).join(' ');
   var expectedOutput = "3 21 2 212 126"; // id 0x15 is 21
   expect(output).toBe(expectedOutput);
 });
@@ -79,31 +94,31 @@ test('encode negative int16', () => {
 test('encode negative int32', () => {
   var input = "setting_name_6: -999";
   var encoded = EncodeTrackerSettings(input);
-  var output = DecodeTrackerSettings(encoded).join(' ');
+  var output = unpackSetting(encoded).join(' ');
   var expectedOutput = "3 22 4 25 252 255 127"; // id 0x16 is 22
   expect(output).toBe(expectedOutput);
 });
 
-test('encode float', () => {    // TODO test this
+test('encode float', () => {
   var input = "setting_name_7: 9.5";
   var encoded = EncodeTrackerSettings(input);
-  var output = DecodeTrackerSettings(encoded).join(' ');
-  var expectedOutput = "3 23 4 9.5"; // id 0x17 is 23
+  var output = unpackSetting(encoded).join(' ');
+  var expectedOutput = "3 23 4 9 0 136 19"; // id 0x17 is 23, 136 and 19 is little endian for 5000 (which is 0,5 on 4 places)
   expect(output).toBe(expectedOutput);
 });
 
 test('encode negative float', () => {
   var input = "setting_name_7: -9.5";
   var encoded = EncodeTrackerSettings(input);
-  var output = DecodeTrackerSettings(encoded).join(' ');
-  var expectedOutput = "3 23 4 -9.5"; // id 0x17 is 23
+  var output = unpackSetting(encoded).join(' ');
+  var expectedOutput = "3 23 4 247 255 136 19"; // id 0x17 is 23
   expect(output).toBe(expectedOutput);
 });
 
 test('encode bool', () => {
   var input = "setting_name_9: true";
   var encoded = EncodeTrackerSettings(input);
-  var output = DecodeTrackerSettings(encoded).join(' ');
+  var output = unpackSetting(encoded).join(' ');
   var expectedOutput = "3 25 1 1"; // id 0x19 is 25
   expect(output).toBe(expectedOutput);
 });
@@ -112,7 +127,7 @@ test('encode string', () => {
   var input = "setting_name_8: testing string";
   var stringAsChars = convertStringToChars("testing string").join(' ');
   var encoded = EncodeTrackerSettings(input);
-  var output = DecodeTrackerSettings(encoded).join(' ');
+  var output = unpackSetting(encoded).join(' ');
   var expectedOutput = "3 24 14 ";  // id 0x18 is 24
   expectedOutput = expectedOutput.concat(stringAsChars); // testing string as char array is 116, 101, 115, 116, 105, 110, 103, 32, 115, 116, 114, 105, 110, 103
   expect(output).toBe(expectedOutput);
